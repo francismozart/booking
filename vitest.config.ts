945e7d920793c7f7a39276bbc9cf04771641aa4c
Francis Andrade <francis.andrade@trustly.com>
2023-12-12 23:03:43 -0300

    chore: Refactor test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    global: true,
+    globals: true,
     environment: "jsdom",
   },
 });
